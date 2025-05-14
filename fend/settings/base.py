1d2016b61a5ea44d1bec50c8f25145e9c87ce84f
Sameer Himati <sameerhimati98@gmail.com>
2025-05-13 19:14:35 -0500

    secuirty changes

--- fend/settings/base.py
+++ fend/settings/base.py
@@ -124,6 +124,8 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
+DEFAULT_FILE_STORAGE = 'django.core.files.storage.FileSystemStorage'
+
 # Default primary key field type
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
